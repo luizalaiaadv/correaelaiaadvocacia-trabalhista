6cdc371691e203ad47879c25512781e6b0ea9032
maxinelobato <limabarreto.org@gmail.com>
2026-06-02 15:30:06 -0300

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -393,7 +393,7 @@ export default function RootLayout({
         <Analytics />
         <Script
           id="gtm"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
